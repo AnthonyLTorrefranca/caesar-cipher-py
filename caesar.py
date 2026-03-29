b246c41c195b014da90ccc64d28950a00a4908c6
AnthonyLTorrefranca <144714043+AnthonyLTorrefranca@users.noreply.github.com>
2026-03-29 17:46:05 +0800

    refactor: complete alphabet string and implement match-case logic
    Refactored caesar_logic to use Python's match-case for cleaner input handling.

--- caesar.py
+++ caesar.py
@@ -1,20 +1,33 @@
-alphabets = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",]
+alphabets = "abcdefghijklmnopqrstuvwxyz"
+print("Welcome to caesar cipher game where we encrypt or decrypt your message to specific number!")
 
-print("Welcome to Caesar Cipher where we'll encrypt a message for you")
-while True:
-    shift_number = input("Input a number for your message to shift! ")
-    if not shift_number.isdigit():
-        print("Pls enter a valid number")
-    else:
-        break
-while True:
-    en_or_dec = input("Type 'Enc' to Encrypt or 'Dec' to decrypt: ").lower()
-    if en_or_dec not in ['enc', 'dec']:
-        print("Uh oh try again:")
-    else:
-        print("Next we'll proceed with the msg")
-        break
-while True:
-    msg = input("The message! ").lower()
+def caesar_logic():
+    def encrypt():
+        enc_msg = input("Input the msg you want to encrypt: ")
+        print(f"Encrypting the message...")
 
-    print(msg)
+    def decrypt():
+        dec_msg = input("Input the msg you want to decrypt: ")
+        print(f"Decrypting the message...")
+    while True:
+        user_choice = input("Choose between enc for encryption and dec for decryption: ").lower()
+
+        match user_choice:
+            case "enc":
+                encrypt()
+                break
+            case "dec":
+                decrypt()
+                break
+            case _:
+                print(f"{user_choice} is invalid! either enc or dec only.")
+
+    else:
+        dec_message = input("Input the message you want to decrypt: ")
+        while True:
+            if dec_message.isdigit():
+                print("Digit isn't allowed! ")
+                dec_message = input("Input the message you want to decrypt: ")
+            else:
+                break
+caesar_logic()
